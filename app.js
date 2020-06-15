ece2bbb7de6f0de583eff7119d648a464745ee76
Agbo betty <43671511+AgboBetty@users.noreply.github.com>
2020-06-15 23:22:01 +0100

    included body parser

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
 app.use(cookieParser());
 app.use(express.json())
 
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 mongoose.connect('mongodb://localhost:27017/bpp',{useNewUrlParser : true,  useUnifiedTopology: true },()=>{
 console.log('successfully connected to database');
 });
  
+
+
 const contractorRouter = require('./routes/Contractor');
 app.use('/contractor', contractorRouter);
+
+
 
 app.listen(5000,()=>{
 console.log('express server started')
